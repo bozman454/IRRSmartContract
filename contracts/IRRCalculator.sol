ed35ce4bcc5a48dda68fa8ba44225dc014ee5372
bozman454 <bozamn454@gmail.com>
2022-01-26 13:16:21 -0500

    added tests

--- contracts/IRRCalculator.sol
+++ contracts/IRRCalculator.sol
@@ -1,60 +1,83 @@
 pragma solidity ^0.5.0;
 
+//used for testing (very helpful)
 import "hardhat/console.sol";
+//used for math w/ over + under flow protection
 import "@hq20/fixidity/contracts/FixidityLib.sol";
+//used to calculate exponents w/ over + under flow protection
 import "@hq20/fixidity/contracts/ExponentLib.sol";
 
-contract IRRCalculator  {
-    int IRR = 0;
-    uint constant p = 10 ** 5;
-
-    struct Analyist {
+contract IRRCalculator {
+    //employee struct for handlng roles / sender origin
+    struct Employees {
+        string role;
         address origin;
     }
+    //map for easy access
+    mapping(address => Employees) public employees;
     address public emp;
-    mapping(address => Analyist) public employees;
+    //stores irr
+    int256 IRR = 0;
+    // scale constant
+    uint256 constant p = 10**5;
 
-    constructor(uint _rate, int[] memory _values) public {
-        require(_rate > 0, "Guess rate cannot be negative");
+    //takes a scaled rate, array of integers, role from sender
+    constructor(
+        uint256 _rate,
+        int256[] memory _values,
+        string memory _role
+    ) public {
         //take sender's info
         emp = msg.sender;
+        //assign origin as sender's address
         employees[emp].origin = emp;
+        //assign sender's role
+        employees[emp].role = _role;
 
-        _rate = _rate * uint(p);
-        for(uint i = 0; i < _values.length; i++){
-            _values[i] = _values[i] * int(p);
+        //scale rate
+        //_rate = _rate * uint256(p);
+        for (uint256 i = 0; i < _values.length; i++) {
+            //scale each entry in _values
+            _values[i] = _values[i] * int256(p);
         }
 
-
+        //calculate IRR given inputs
         IRR = guessIRR(_rate, _values);
-        
-       
-        
     }
 
-    function guessIRR(uint _rate, int[] memory _values) internal returns (int){
+    //TODO: figure out how to scale fixed point after exponents
+    function guessIRR(uint256 _rate, int256[] memory _values)
+        internal
+        returns (int256)
+    {
+        //check sender's role
+        //use keccak256 to turn into bytes for comparison
+        require(
+            keccak256(bytes(employees[msg.sender].role)) ==
+                keccak256(bytes("financial analyst")),
+            "Only financial analyists can access this interface"
+        );
 
+        //ensure values is not empty
+        require(_values.length > 0, "Values entered cannot be empty");
 
-            require(msg.sender == emp, "Only financial analyists can access this interface");
-            require(_rate > 0, "Guess rate cannot be negative");
-            require(_values.length > 0, "Values entered cannot be empty");
+        //temporary variable to hold calculation
+        int256 irr = 0;
 
-            int irr = 0;
-            uint p = 10 ** 5;
-            for(uint i = 0; i < _values.length; i++){
-            uint denom = (_rate ) ** i;
-            //int x = FixidityLib.divide(int(denom), int(i) * 10);
-            //console.logInt(x);
-            int npv = FixidityLib.multiply((_values[i]), FixidityLib.reciprocal(int(denom)) );
+        for (uint256 i = 0; i < _values.length; i++) {
 
+            uint256 denom = (_rate)**i;
+            //use FixidityLib to do math with over / under flow protection
+            int256 npv = FixidityLib.multiply(
+                (_values[i]),
+                FixidityLib.reciprocal(int256(denom))
+            );
             irr = irr + npv;
-            }
-            return irr;
         }
+        return irr;
+    }
 
-    function getIRR() public view returns (int){
+    function getIRR() public view returns (int256) {
         return IRR;
     }
-
-    
 }
--- test/irr-test.js
+++ test/irr-test.js
@@ -1,32 +1,47 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const truffleAssert = require('truffle-assertions')
+
+const values = [
+    -100.00,
+    14.00,
+    25.00,
+    16.00,
+    12.00,
+    79.00,
+    36.00,
+    42.00
+];
+
+//scaled percentage
+const irrrate = 19883        
+const role = "financial analyst"
+
 
 describe("IRRCalculator", function () {
-    it("Should return the number 69", async function () {
-
+    it("Should return the number correct IRR guess (.00151857)", async function () {
         const IRR = await hre.ethers.getContractFactory("IRRCalculator");
-
-        const values = [
-            -100.00,
-            14.00,
-            25.00,
-            16.00,
-            12.00,
-            79.00,
-            36.00,
-            42.00
-        ];
-
-        const irrrate = 19883
-        
-        const scale = 10000
-
-        var newValues = [];
-        values.map( v => newValues.push(v * scale))
-        const rate = await IRR.deploy(irrrate, values);
-
+        const rate = await IRR.deploy(irrrate, values, role);
         await rate.deployed();
-        expect(await rate.getIRR() / Math.pow(10, 24)).to.equal(10);
-        
+        expect(await rate.getIRR() / Math.pow(10, 5)).to.equal(.00151857);
     })
 }) 
+
+describe("IRRCalculator", function (){
+    it("Should break becuase bad input", async function (){
+        
+        const IRR = await hre.ethers.getContractFactory("IRRCalculator");
+        await truffleAssert.reverts(IRR.deploy(irrrate, [], role),
+         'Values entered cannot be empty');
+        
+    })
+})
+
+
+describe("IRRCalculator", function (){
+    it("Should break becuase bad role", async function () {
+        const IRR = await hre.ethers.getContractFactory("IRRCalculator");
+        await truffleAssert.reverts(IRR.deploy(irrrate, values, "Cashier"),
+        'Only financial analyists can access this interface');
+    })
+})
